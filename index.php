dd7e679e76ff524a4fdf22762ba083edfadf7eaf
silveur <silvere.letellier@gmail.com>
2014-05-20 18:21:30 +0100

    google ana

--- index.php
+++ index.php
@@ -9,47 +9,54 @@
 
 	<script src="js/scripts.js"></script>
 	<script>
-	var windowWidth; var windowHeigth; var videoIndex; var videoOffset; 
-	var videoUrlMp4; var videoUrlWebm; var videoUrlOgg;
-	function getWindowBoundaries()
-	{
-		var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0],
-		width = w.innerWidth || e.clientWidth || g.clientWidth,
-		heigth = w.innerHeight|| e.clientHeight|| g.clientHeight;
-		windowWidth = width; windowHeigth = heigth;
-		debug('Height:' + heigth.toString() + ' Width: ' + width.toString());
-	}
-	function getRandomVideo()
-	{
-		videoIndex = Math.floor(1 + Math.random()*3);
-		if (videoIndex == 1) videoOffset = Math.floor(Math.random() * 218);
-		else if (videoIndex ==2 ) videoOffset = Math.floor(Math.random() * 141);
-		videoUrlMp4 = "./videos/" + videoIndex + ".mp4#t=" + videoOffset + " type='video/mp4'> ";
-		videoUrlWebm = "./videos/" + videoIndex + ".webm#t=" + videoOffset + " type='video/webm'> ";
-		videoUrlOgg = "./videos/" + videoIndex + ".ogv#t=" + videoOffset + " type='video/ogg'> ";
-	}
-	function printVideo()
-	{
-		var printVideo = 	"<video autoplay loop id='bgvid'> " +
-							"<source src=" + videoUrlMp4 + 
-							"<source src=" + videoUrlWebm +
-							"<source src=" + videoUrlOgg + 
-							"</video>" ; 
-		document.getElementById("bgvid").innerHTML = printVideo;
-	}
-	function printLinks()
-	{
-		var x = Math.floor((windowWidth/4)*3).toString() + "px";
-		var y = Math.floor(windowHeigth/5).toString() + "px";
-		document.getElementById("textID").style.visibility = "visible";
-		document.getElementById("textID").style.margin = y + " 0px 0px " + x ;
-	}
-	function printH1()
-	{
-		var position = (Math.floor((windowHeigth/9))).toString() + "px 0px 0px 0px";
-		document.getElementById("header").style.margin = position;
-	}
- 	
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-51181390-1', 'nummermusic.com');
+		ga('send', 'pageview');
+	</script>
+	<script>
+		var windowWidth; var windowHeigth; var videoIndex; var videoOffset; 
+		var videoUrlMp4; var videoUrlWebm; var videoUrlOgg;
+		function getWindowBoundaries()
+		{
+			var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0],
+			width = w.innerWidth || e.clientWidth || g.clientWidth,
+			heigth = w.innerHeight|| e.clientHeight|| g.clientHeight;
+			windowWidth = width; windowHeigth = heigth;
+			debug('Height:' + heigth.toString() + ' Width: ' + width.toString());
+		}
+		function getRandomVideo()
+		{
+			videoIndex = Math.floor(1 + Math.random()*3);
+			if (videoIndex == 1) videoOffset = Math.floor(Math.random() * 218);
+			else if (videoIndex ==2 ) videoOffset = Math.floor(Math.random() * 141);
+			videoUrlMp4 = "./videos/" + videoIndex + ".mp4#t=" + videoOffset + " type='video/mp4'> ";
+			videoUrlWebm = "./videos/" + videoIndex + ".webm#t=" + videoOffset + " type='video/webm'> ";
+			videoUrlOgg = "./videos/" + videoIndex + ".ogv#t=" + videoOffset + " type='video/ogg'> ";
+		}
+		function printVideo()
+		{
+			var printVideo = 	"<video autoplay loop id='bgvid'> " +
+								"<source src=" + videoUrlMp4 + 
+								"<source src=" + videoUrlWebm +
+								"<source src=" + videoUrlOgg + 
+								"</video>" ; 
+			document.getElementById("bgvid").innerHTML = printVideo;
+		}
+		function printLinks()
+		{
+			var x = Math.floor((windowWidth/4)*3).toString() + "px";
+			var y = Math.floor(windowHeigth/5).toString() + "px";
+			document.getElementById("textID").style.visibility = "visible";
+			document.getElementById("textID").style.margin = y + " 0px 0px " + x ;
+		}
+		function printH1()
+		{
+			var position = (Math.floor((windowHeigth/9))).toString() + "px 0px 0px 0px";
+			document.getElementById("header").style.margin = position;
+		}
 	</script>
 
 	<body  onclick="printLinks()">
