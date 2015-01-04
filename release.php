5d6e5949fc3716eda2bc6420689abd691f298a7f
Silvere Letellier <silvere.letellier@gmail.com>
2015-01-04 17:39:55 +0000

    added success and release page

--- release.php
+++ release.php
@@ -2,27 +2,45 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/release.css">
 		<link rel="icon" type="image/jpg" href="images/favicon14px.jpg">
 		<title>NUMMER MUSIC</title>
 	</head>
 	<script src="js/scripts.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<body>
-		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="TKS5Y8GYT23PL">
-<table>
-<tr><td><input type="hidden" name="on0" value="Shipping options">Shipping options</td></tr><tr><td><select name="os0">
-	<option value="UK">UK £12.50 GBP</option>
-	<option value="Europe">Europe £14.00 GBP</option>
-	<option value="Rest of the world">Rest of the world £18.00 GBP</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="GBP">
-<input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-</form>
+		
+		<?php
+			$releaseCAT = htmlspecialchars($_GET["release"]);
 
+			$con=mysqli_connect("localhost","root","root", "Releases");
+			if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+			$result = mysqli_query($con,"SELECT * FROM Releases WHERE CatalogueNumber = '$releaseCAT'");
+			$row = mysqli_fetch_array($result);
+
+			echo $row['VideoLink'] ;
+			echo "</br>Release name: " . $row['ReleaseName'];
+			echo "</br>Record label: " . $row['RecordLabel'];
+			echo "</br>Catalogue number: " . $row['CatalogueNumber'];
+			echo "</br>Inventory: " . $row['Inventory'];
+			if($row['Inventory'] == 0) echo "</br>Sold out";
+			else
+			{
+				?>
+     				<form action="action_page.php">
+					First and last name:<br>
+					<input type="text" name="firstname" value="First Name">
+					<br>
+					
+					<input type="submit" value="Submit">
+					</form>
+				<?php 
+
+			}
+			mysql_close($con);
+		?>
+
+		<p> For grouped orders please contact us by email: contact@nummermusic.com </p>
 	</body>
 </html>
