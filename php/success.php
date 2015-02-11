f129b835d916fd1c3eb8ceae9bab89533fcb1b58
silveur <silvere.letellier@gmail.com>
2015-02-11 17:28:34 +0000

    confirming the order

--- php/success.php
+++ php/success.php
@@ -34,24 +34,36 @@
 			$row = mysqli_fetch_array($result);
 			$newInventory = $row['Inventory'] - 1;
 			mysqli_query($con,"UPDATE Releases SET Inventory='$newInventory' WHERE CatalogueNumber='$releaseCAT'");
-			
-			mysql_close($con);
 
-			$to      = $EMAIL;
-			$subject = 'Thanks for your order' . ' - ' . $releaseCAT;
-			$message = "Thanks for your order, we will give you a heads up when it's on its way :)" . "\r\n" . "Cheers" . "\r\n" . "Nummer";
-			$headers = 'From: Nummer Music <contact@nummermusic.com>' . "\r\n" .
-			    'Reply-To: contact@nummermusic.com' . "\r\n" .
-			    'X-Mailer: PHP/' . phpversion();
-			mail($to, $subject, $message, $headers);
+			$confirmation = file_get_contents('https://api-3t.paypal.com/nvp?TOKEN=' . $token . '&PAYMENTREQUEST_0_AMT=' .$AMT . '&PayerID=' . $PayerID. '&VERSION=106.0&&PAYMENTREQUEST_0_PAYMENTACTION=SALE&SIGNATURE=AXYVGbf-S.vpK0cdOArIz.pHtQkyAL2coxQtEK20Oi-L1p4KnSsIwULe&METHOD=DoExpressCheckoutPayment&PWD=HKTYCLKWULYA6LP3&USER=contact_api1.nummermusic.com&PAYMENTREQUEST_0_CURRENCYCODE=GBP');
+			parse_str($confirmation);
+
+			if ($ACK=='Success')
+			{
+				echo "<center> <H2>Thanks for your order. We will get in touch with you very soon</H2>";
+				mysql_close($con);
+
+				$to      = $EMAIL;
+				$subject = 'Thanks for your order' . ' - ' . $releaseCAT;
+				$message = "Thanks for your order, we will give you a heads up when it's on its way :)" . "\r\n" . "Cheers" . "\r\n" . "Nummer";
+				$headers = 'From: Nummer Music <contact@nummermusic.com>' . "\r\n" .
+				    'Reply-To: contact@nummermusic.com' . "\r\n" .
+				    'X-Mailer: PHP/' . phpversion();
+				mail($to, $subject, $message, $headers);
+			}
+			else
+			{
+				echo "<center> <H2>An error has occured. Sorry for the inconvenience</H2>";
+				$to      = 'contact@nummermusic.com';
+				$subject = 'ORDER CANCELLED' . ' - ' . $releaseCAT;
+				$message = $confirmation;
+				$headers = 'From: Nummer Music <no-reply@nummermusic.com>' . "\r\n" .
+				    'Reply-To: contact@nummermusic.com' . "\r\n" .
+				    'X-Mailer: PHP/' . phpversion();
+				mail($to, $subject, $message, $headers);
+			}
+			echo '<a href="http://www.nummermusic.com">Back to Nummer Music</a>';
 		?>
-
-		<center> <H2>Thanks for your order. We will get in touch with you very soon</H2>
-
-			<H3>Redirecting to Nummer Music...</H3></center>
-			<script type="text/javascript">
-			setTimeout("location.href = 'http://www.nummermusic.com';",2500);
-			</script>
 	</body>
 </html>
 
