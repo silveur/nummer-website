c704ff780afd884940e072d864eeef91e7e4332c
silveur <silvere.letellier@gmail.com>
2015-03-05 23:44:16 +0000

    updated for linux config

--- php/app.php
+++ php/app.php
@@ -1,14 +1,19 @@
 <?php
-	$home = getenv("HOME");
-	$phpRoot = $_SERVER['DOCUMENT_ROOT'];
-	$settings = file_get_contents($home . '/settings.json', true);
-	$settings = json_decode($settings);
-
 	$os = PHP_OS;
 	if ($os == "Darwin")
+	{
+		$home = getenv("HOME");
+		$settings = file_get_contents($home . '/settings.json', true);
+		$settings = json_decode($settings);
 		$settings = $settings->local;
+	}
 	else
+	{
+		$home = 'home/silveur';
+		$settings = file_get_contents($home . '/settings.json', true);
+		$settings = json_decode($settings);
 		$settings = $settings->production;
+	}
 
 	$con = mysqli_connect( "localhost", $settings->mysql->user, $settings->mysql->password, "NummerWebsite" );
 
